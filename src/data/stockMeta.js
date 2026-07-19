8c70a64e4cca488045f6d9a4b2125814200dfea0
breakoutpro <stocksbuddyofficial@gmail.com>
2026-07-19 07:51:40 +0530

    Add files via upload

---
+++ src/data/stockMeta.js
@@ -0,0 +1,21 @@
+var STOCK_META = {
+  RELIANCE:{pe:28.4,pb:2.1,mcap:"19.2L Cr",high52:"3024",low52:"2220"},
+  TCS:{pe:31.2,pb:13.4,mcap:"13.2L Cr",high52:"4592",low52:"3056"},
+  HDFCBANK:{pe:19.8,pb:2.8,mcap:"13.1L Cr",high52:"1880",low52:"1363"},
+  ICICIBANK:{pe:18.4,pb:3.2,mcap:"9.1L Cr",high52:"1362",low52:"972"},
+  INFY:{pe:24.6,pb:7.8,mcap:"6.5L Cr",high52:"1974",low52:"1307"},
+  WIPRO:{pe:22.1,pb:4.2,mcap:"2.5L Cr",high52:"571",low52:"378"},
+  TATAMOTORS:{pe:8.2,pb:3.1,mcap:"3.5L Cr",high52:"1179",low52:"724"},
+  SBIN:{pe:11.2,pb:1.8,mcap:"7.2L Cr",high52:"912",low52:"543"},
+  AXISBANK:{pe:14.8,pb:2.4,mcap:"3.6L Cr",high52:"1340",low52:"882"},
+  BAJFINANCE:{pe:32.4,pb:6.8,mcap:"4.5L Cr",high52:"7830",low52:"5924"},
+  MARUTI:{pe:26.1,pb:4.8,mcap:"4.0L Cr",high52:"13680",low52:"9832"},
+  SUNPHARMA:{pe:34.2,pb:5.6,mcap:"4.0L Cr",high52:"1960",low52:"1179"},
+  LT:{pe:32.1,pb:5.4,mcap:"4.7L Cr",high52:"3978",low52:"2673"},
+  HCLTECH:{pe:27.3,pb:8.2,mcap:"5.1L Cr",high52:"1978",low52:"1235"},
+  TATASTEEL:{pe:12.4,pb:1.8,mcap:"2.1L Cr",high52:"184",low52:"119"},
+  ADANIENT:{pe:45.2,pb:8.9,mcap:"3.3L Cr",high52:"3743",low52:"2017"},
+};
+
+export { STOCK_META };
+

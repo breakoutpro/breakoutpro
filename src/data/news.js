c8b10dc3a23cb8859a9540d95bc4c8fb8d0bd351
breakoutpro <stocksbuddyofficial@gmail.com>
2026-07-24 18:54:10 +0530

    Delete src/data/news.js

--- src/data/news.js
+++
@@ -1,7 +0,0 @@
-export var NEWS = [
-  {title:"RBI holds rates steady at 6.5%, signals neutral stance",time:"2h ago",cat:"Policy",up:true},
-  {title:"NIFTY crosses 22,500 on strong FII inflows of Rs 4,200 Cr",time:"3h ago",cat:"Markets",up:true},
-  {title:"Q4 results: IT sector beats estimates by 8%",time:"4h ago",cat:"Results",up:true},
-  {title:"Crude oil rises 2.3% on OPEC supply cut",time:"5h ago",cat:"Global",up:false},
-  {title:"SEBI proposes new margin rules effective July",time:"6h ago",cat:"SEBI",up:false},
-];
